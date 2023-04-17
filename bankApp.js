b7a84356d8d02abf55ca619daab82fb29d2585f1
Francesco Guglielmi <francescoguglielmi@Francescos-MacBook-Air.local>
2023-04-17 20:17:21 +0100

    test driven dataValidation function test driven dataValidate function

--- bankApp.js
+++ bankApp.js
@@ -1,20 +1,38 @@
 const account = {
-  currentBalance: 0,
+  accountId: 1,
+  currentBalance: 1000,
   dateCredited: [],
   dateDebited: [],
   credit: [],
   debit: []
-}
+};
 
-export function withdraw() {
+function dataValidate(amount) {
+  if (!(typeof amount === 'number')) {
+    throw 'The input must be a number';
+  } else if (amount <= 0) {
+    throw 'The input must be positive';
+  }
+};
 
-}
+function withdraw(amount) {
 
-export function deposit() {
+};
 
-}
+function deposit(amount) {
 
-export function statement() {
+};
 
-}
+function statement() {
+
+};
+
+module.exports = {
+  dataValidate,
+  withdraw,
+  deposit,
+  statement
+};
+
+
 
--- bankApp.test.js
+++ bankApp.test.js
@@ -1,1 +1,26 @@
-import {withdraw, deposit, statement} from "./bankApp.js"
+const App = require('./bankApp.js');
+
+describe('dataValidate', () => {
+
+  beforeEach(() => {
+    const account = {
+      id: 2,
+      currentBalance: 1000,
+      dateCredited: [],
+      dateDebited: [],
+      credit: [],
+      debit: []
+    }
+  });
+
+  test(('throws an error if the amount is negative'), () => {
+    expect(() => {
+      App.dataValidate(-600)}).toThrow('The input must be positive');
+  });
+
+  test(('throws an error if the amount is not a number'), () => {
+    expect(() => {
+      App.dataValidate('hello')}).toThrow('The input must be a number');
+  });
+
+});
